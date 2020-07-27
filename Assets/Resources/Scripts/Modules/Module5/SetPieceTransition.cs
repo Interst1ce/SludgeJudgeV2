a2353829c32ad6f1b28cb069cfce4b98f33d7e62
Interst1ce <intersticegamesdev@gmail.com>
2020-07-27 13:58:58 -0500

    Update SetPieceTransition.cs

--- Assets/Resources/Scripts/Modules/Module5/SetPieceTransition.cs
+++ Assets/Resources/Scripts/Modules/Module5/SetPieceTransition.cs
@@ -7,28 +7,13 @@ public class SetPieceTransition : MonoBehaviour {
     public GameObject setPieceTwo;
     public GameObject setPieceThree;
 
-    List<Material> GetTransitionMaterials(GameObject setpiece) {
-        List<Material> materials = new List<Material>();
-
-        foreach (Renderer renderer in setpiece.GetComponentsInChildren<Renderer>()) {
-            if (!materials.Contains(renderer.material)) materials.Add(renderer.material);
-        }
-
-        return materials;
-    }
-
-
     public void TransitionOne() {
-        //List<Material> fadeOut = GetTransitionMaterials(setPieceOne);
         Debug.Log("Starting TransitionOne");
         StartCoroutine(ScaleTransition(setPieceTwo,setPieceOne,35.5f));
         Debug.Log("TransitionOne started");
     }
 
-    
-
     public void TransitionTwo() {
-        //List<Material> fadeIn = GetTransitionMaterials(setPieceThree);
         Debug.Log("Starting TransitionTwo");
         StartCoroutine(ScaleTransition(setPieceThree,null,92f));
         Debug.Log("TransitionTwo started");
@@ -57,40 +42,6 @@ public class SetPieceTransition : MonoBehaviour {
             while (t < 1) {
                 inSetPiece.transform.localScale = Vector3.Lerp(Vector3.zero,Vector3.one,t);
                 t += Time.deltaTime;
-                yield return null;
-            }
-        }
-    }
-
-    IEnumerator AlphaTransition(List<Material> fadeIn, GameObject inSetPiece, List<Material> fadeOut = null, GameObject outSetPiece = null, float delay = 0) {
-        float t = 0;
-        yield return new WaitForSeconds(delay);
-        if (fadeOut != null) {
-            while (t < 1) {
-                foreach (Material material in fadeOut) {
-                    Color color = material.color;
-                    color.a = Mathf.Lerp(255,0,t);
-                    material.color = color;
-                    t += Time.deltaTime;
-                }
-                yield return null;
-            }
-            outSetPiece.SetActive(false);
-        }
-
-        if(inSetPiece != null) inSetPiece.SetActive(true);
-
-        if (fadeIn != null) {
-            t = 0;
-            while (t < 1) {
-                foreach (Material material in fadeOut) {
-                    Color color = material.color;
-                    if (material.name == "Zone123 Plane") {
-                        color.a = Mathf.Lerp(0,190,t);
-                    } else color.a = Mathf.Lerp(0,255,t);
-                    material.color = color;
-                    t += Time.deltaTime;
-                }
                 yield return null;
             }
         }
