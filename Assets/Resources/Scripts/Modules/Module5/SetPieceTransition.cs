640ce74faef4d001872af3c539ce0d1d7f71bfbd
Interst1ce <intersticegamesdev@gmail.com>
2020-07-28 13:59:59 -0500

    Chloramination Transition Update
    Replaced scripts with animations, need to fix zero scaling issue

--- Assets/Resources/Scripts/Modules/Module5/SetPieceTransition.cs
+++ Assets/Resources/Scripts/Modules/Module5/SetPieceTransition.cs
@@ -8,36 +8,28 @@ public class SetPieceTransition : MonoBehaviour {
     public GameObject setPieceThree;
 
     public void TransitionOne() {
-        Debug.Log("Starting TransitionOne");
-        StartCoroutine(ScaleTransition(setPieceTwo,setPieceOne,35.5f));
-        Debug.Log("TransitionOne started");
+        StartCoroutine(ScaleTransition(setPieceTwo,setPieceOne,36.667f));
     }
 
     public void TransitionTwo() {
-        Debug.Log("Starting TransitionTwo");
-        StartCoroutine(ScaleTransition(setPieceThree,null,92f));
-        Debug.Log("TransitionTwo started");
+        StartCoroutine(ScaleTransition(setPieceThree,null,95f));
     }
 
     IEnumerator ScaleTransition(GameObject inSetPiece, GameObject outSetPiece, float delay = 0) {
-        Debug.Log("Waiting for " + delay + " seconds");
         yield return new WaitForSeconds(delay);
         float t = 0;
         if (outSetPiece != null) {
-            Debug.Log("Scaling object down");
             while(t < 1) {
                 outSetPiece.transform.localScale = Vector3.Lerp(Vector3.one,Vector3.zero,t);
+                Debug.Log("" + outSetPiece.transform.localScale);
                 t += Time.deltaTime;
                 yield return null;
             }
-            Debug.Log("Deactivating object");
             outSetPiece.SetActive(false);
         }
         t = 0;
         if (inSetPiece != null) {
-            Debug.Log("Activating object");
             inSetPiece.SetActive(true);
-            Debug.Log("Scaling object up");
             inSetPiece.transform.localScale = Vector3.zero;
             while (t < 1) {
                 inSetPiece.transform.localScale = Vector3.Lerp(Vector3.zero,Vector3.one,t);
